2df372e00632db354508c5aca103c1bffcf3b2e1
thiagop91 <thiago-tsunami@hotmail.com>
2025-03-23 21:28:52 -0300

    Agenda 5

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,11 +4,26 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+        double _quantidade;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+                _descricao = value;
+            }
+        }
         public double Quantidade { get; set; }
-        public double Preco {  get; set; }
+        public double Preco { get; set; }
         public double Total { get => Quantidade * Preco; }
+
     }
 }
--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -9,9 +9,9 @@ public partial class ListaProduto : ContentPage
     ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
     ObservableCollection<Produto> listaFiltrada = new ObservableCollection<Produto>();
     public ListaProduto()
-	{
-		InitializeComponent();
-	
+    {
+        InitializeComponent();
+
         lst_produtos.ItemsSource = lista;
         lst_produtos.ItemsSource = listaFiltrada;
 
@@ -19,9 +19,18 @@ public partial class ListaProduto : ContentPage
 
     protected async override void OnAppearing()
     {
-        List<Produto> tmp = await App.Db.GetAll();
+        try
+        {
+            lista.Clear();
 
-        tmp.ForEach(i => lista.Add(i));
+            List<Produto> tmp = await App.Db.GetAll();
+
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
@@ -40,20 +49,27 @@ public partial class ListaProduto : ContentPage
 
     private async void txt_seacrh_TextChanged(object sender, TextChangedEventArgs e)
     {
+        try
+        {
+            string q = e.NewTextValue;
 
-        string q = e.NewTextValue;
+            lista.Clear();
+            listaFiltrada.Clear();
 
-        lista.Clear();
-        listaFiltrada.Clear();
+            List<Produto> tmp = await App.Db.Search(q);
 
-        List<Produto> tmp = await App.Db.Search(q);
+            tmp.ForEach(i => lista.Add(i));
+            tmp.ForEach(i => listaFiltrada.Add(i));
 
-        tmp.ForEach(i => lista.Add(i));
-        tmp.ForEach(i => listaFiltrada.Add(i));
-
-        string searchText = e.NewTextValue?.ToLower() ?? string.Empty;
-        FiltrarProdutos(searchText);
+            string searchText = e.NewTextValue?.ToLower() ?? string.Empty;
+            FiltrarProdutos(searchText);
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
+
 
     private void FiltrarProdutos(string searchText)
     {
@@ -83,6 +99,7 @@ public partial class ListaProduto : ContentPage
 
     private async void MenuItem_Clicked(object sender, EventArgs e)
     {
+        try
         {
             MenuItem selecionado = sender as MenuItem;
 
@@ -97,5 +114,27 @@ public partial class ListaProduto : ContentPage
                 lista.Remove(p);
             }
         }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        try
+        {
+            Produto p = e.SelectedItem as Produto;
+
+            Navigation.PushAsync(new Views.EditarProduto
+            {
+                BindingContext = p,
+            });
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+
     }
 }
